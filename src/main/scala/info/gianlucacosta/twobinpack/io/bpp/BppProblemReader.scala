ac837a592e254ee86ce3aceab06b9ec6542b0cf5
Gianluca Costa <gianluca@gianlucacosta.info>
2017-02-06 05:00:06 +0100

    Add I/O support for BPP problems

---
+++ src/main/scala/info/gianlucacosta/twobinpack/io/bpp/BppProblemReader.scala
@@ -0,0 +1,108 @@
+/*^
+  ===========================================================================
+  TwoBinKernel
+  ===========================================================================
+  Copyright (C) 2016-2017 Gianluca Costa
+  ===========================================================================
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public
+  License along with this program.  If not, see
+  <http://www.gnu.org/licenses/gpl-3.0.html>.
+  ===========================================================================
+*/
+
+package info.gianlucacosta.twobinpack.io.bpp
+
+import java.io.BufferedReader
+import java.time.Duration
+import java.util.UUID
+
+import info.gianlucacosta.helios.io.DecoratorReader
+import info.gianlucacosta.twobinpack.core._
+
+
+/**
+  * Reader providing a dedicated method for reading BPP problems from text files
+  *
+  * @param sourceReader
+  */
+class BppProblemReader(sourceReader: BufferedReader) extends DecoratorReader(sourceReader) {
+  def readBppProblem(
+                      initialFrameWidth: Int,
+                      timeLimitOption: Option[Duration],
+                      name: String,
+                      id: UUID
+                    ): Problem = {
+    val frameLine =
+      sourceReader.readLine().trim
+
+    val initialFrameHeight =
+      frameLine.toInt
+
+
+    val initialFrameDimension =
+      FrameDimension(
+        initialFrameWidth,
+        initialFrameHeight
+      )
+
+
+    val blockDimensions: Map[BlockDimension, Int] =
+      parseLineBlock(trimmedLine => {
+        val blockWidth =
+          1
+
+        val blockHeight =
+          trimmedLine.split(" ").last.toInt
+
+
+        BlockDimension(
+          blockWidth,
+          blockHeight
+        )
+      })
+        .groupBy(blockdimension => blockdimension)
+        .map {
+          case (blockDimension, groupedBlocks) =>
+            blockDimension -> groupedBlocks.length
+        }
+
+
+    val blockPool =
+      BlockPool.create(
+        canRotateBlocks = false,
+        draftBlocks = blockDimensions
+      )
+
+    val frameTemplate =
+      FrameTemplate(
+        initialFrameDimension,
+        FrameMode.Strip,
+        blockPool,
+        FrameTemplate.SuggestedBlockColorsPool,
+        Problem.SuggestedResolution
+      )
+
+
+    val problem =
+      Problem(
+        frameTemplate,
+        timeLimitOption,
+        name,
+        id
+      )
+
+    require(problem.isBinPacking)
+
+    problem
+  }
+}
---
+++ src/main/scala/info/gianlucacosta/twobinpack/io/bpp/BppProblemWriter.scala
@@ -0,0 +1,66 @@
+/*^
+  ===========================================================================
+  TwoBinKernel
+  ===========================================================================
+  Copyright (C) 2016-2017 Gianluca Costa
+  ===========================================================================
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public
+  License along with this program.  If not, see
+  <http://www.gnu.org/licenses/gpl-3.0.html>.
+  ===========================================================================
+*/
+
+package info.gianlucacosta.twobinpack.io.bpp
+
+import java.io.Writer
+
+import info.gianlucacosta.helios.io.DecoratorWriter
+import info.gianlucacosta.twobinpack.core.Problem
+
+/**
+  * Writer providing a dedicated method for writing BPP problems to text files
+  *
+  * @param targetWriter
+  */
+class BppProblemWriter(targetWriter: Writer) extends DecoratorWriter(targetWriter) {
+  def writeBppProblem(problem: Problem): Unit = {
+    require(problem.isBinPacking)
+
+    targetWriter.write(problem.frameTemplate.initialDimension.height.toString)
+    targetWriter.write("\r\n")
+
+    problem.frameTemplate.blockPool.blocks
+      .toList
+      .sortBy {
+        case (blockDimension, quantity) =>
+          blockDimension
+      }
+      .flatMap {
+        case (blockDimension, quantity) =>
+          List.fill(quantity)(blockDimension)
+      }
+      .zipWithIndex
+      .foreach {
+        case (blockDimension, index) =>
+          val blockOrdinal =
+            index + 1
+
+          targetWriter.write(blockOrdinal.toString)
+          targetWriter.write(" ")
+          targetWriter.write(blockDimension.height.toString)
+          targetWriter.write("\r\n")
+      }
+
+    targetWriter.write("\r\n")
+  }
+}
